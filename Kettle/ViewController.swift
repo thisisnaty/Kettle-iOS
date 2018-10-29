7a1b87ba3e7ffb62d7be7f936bc6b70c7dd3fac2
Natalia <natgarc@microsoft.com>
2018-10-29 11:11:09 -0700

    Initial Commit

---
+++ Kettle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kettle
+//
+//  Created by Natalia Garcia on 10/29/18.
+//  Copyright © 2018 Natalia Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
